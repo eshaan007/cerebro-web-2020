696beb3c8ca82bfb3204ef5b866f4c4e193cd2cb
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-02-25 22:20:38 +0530

    Past Glimpses : Mobile View

--- src/Components/Home/PastGlimpse/PastGlimpse.jsx
+++ src/Components/Home/PastGlimpse/PastGlimpse.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import './PastGlimpse.scss'
 
 const PastGlimpse = () => {
+  const width=window.screen.width;
   return (
     <div className="gallery">
       <h1 className="gallery__title section-title">Past year Glimpses</h1>
       <div className="gallery__images">
         <div className="gallery__images__container">
-          <div className="wide" style={{backgroundImage:"url('./media/botrun.png')" }}></div>
-          <div className="tall" style={{backgroundImage: "url('./media/git.png')"}}></div>
-          <div className="big" style={{backgroundImage: "url('./media/team.png')"}}></div>
+          <div className="wide" style={{backgroundImage:`url('./media/botrun${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="tall" style={{backgroundImage: `url('./media/git${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="big" style={{backgroundImage: `url('./media/team${width<=1000?'_mobile':''}.png')`}}></div>
           <div className="wide" style={{backgroundImage: "url('./media/ideathon.png')"}}></div>
           <div className="tall" style={{backgroundImage: "url('./media/director.png')"}}></div>
-          <div className="" style={{backgroundImage: "url('./media/penumbera.png')"}}></div>
-          <div className="wide" style={{backgroundImage: "url('./media/baymax.png')"}}></div>
-          <div className="" style={{backgroundImage: "url('./media/webhead.png')"}}></div>
+          <div className="" style={{backgroundImage: `url('./media/penumbera${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="wide" style={{backgroundImage: `url('./media/baymax${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="" style={{backgroundImage: `url('./media/webhead${width<=1000?'_mobile':''}.png')`}}></div>
         </div>
       </div>
     </div>
